653043e3d2d192f52fe59927e6e3d1e68b9d307e
Oleksii Masalitin <oleksii.u.masalitin@gmail.com>
2020-08-02 13:32:49 +0300

    write a code with a few mistakes

--- Exam.py
+++ Exam.py
@@ -1,3 +1,17 @@
 def avg(li):
     s=sum(i for i in li)
     return float(s)/len(li)
+li1, li2, li3=[], [], []
+while True:
+    li=input().strip().split(';')
+    for i in range(1,len(li)):
+        li[i]=int(li[i])
+    if li=='':
+        break
+    else:
+        li1.append(int(li[1]))
+        li2.append(int(li[2]))
+        li3.append(int(li[3]))
+        print(avg(li[1:]))
+
+#with open('spepik4.txt', 'r') as inf, open('out4.txt', 'w') as ouf:
