b2da4cdf22cb39b31d2064b991b8dcf60f41b48e
Oleksii Masalitin <oleksii.u.masalitin@gmail.com>
2020-08-02 13:52:02 +0300

    Add text file work and fixed the bug

--- Exam.py
+++ Exam.py
@@ -2,16 +2,16 @@ def avg(li):
     s=sum(i for i in li)
     return float(s)/len(li)
 li1, li2, li3=[], [], []
-while True:
-    li=input().strip().split(';')
-    for i in range(1,len(li)):
-        li[i]=int(li[i])
-    if li=='':
-        break
-    else:
-        li1.append(int(li[1]))
-        li2.append(int(li[2]))
-        li3.append(int(li[3]))
-        print(avg(li[1:]))
-
-#with open('spepik4.txt', 'r') as inf, open('out4.txt', 'w') as ouf:
+with open('stepik6.txt', 'r') as inf, open('out6.txt', 'w') as ouf:
+    while True:
+        li=inf.readline().strip().split(';')
+        for i in range(1,len(li)):
+            li[i]=int(li[i])
+        if len(li)==1:
+            break
+        else:
+            li1.append(int(li[1]))
+            li2.append(int(li[2]))
+            li3.append(int(li[3]))
+            ouf.write(str(avg(li[1:])))
+            ouf.write('\n')
